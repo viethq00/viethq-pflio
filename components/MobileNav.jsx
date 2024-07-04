3e52616545011c852ff9a95b7454aa3a430c5be7
viethq00 <73158024+viethq00@users.noreply.github.com>
2024-07-04 11:02:52 +0700

    update

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -28,13 +28,38 @@ const links = [
 ];
 
 const MobileNav = () => {
+  const pathName = usePathname();
   return (
     <Sheet>
       <SheetTrigger className="flex justify-center items-center">
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
       <SheetContent className="flex flex-col">
-        <div>logo</div>
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              VietHQ
+              <span className="text-accent"> .</span>
+            </h1>
+          </Link>
+        </div>
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`text-xl capitalize hover:text-accent transition-all ${
+                  link.path == pathName
+                    ? "text-accent border-b-2 border-accent"
+                    : ""
+                }`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
       </SheetContent>
     </Sheet>
   );
