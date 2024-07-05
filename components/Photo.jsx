1f07f0d21c604b822eaf1c26f3ccf7a70a4044c1
viethq00 <73158024+viethq00@users.noreply.github.com>
2024-07-05 17:39:06 +0700

    Update Photo.jsx

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -17,7 +17,7 @@ const Photo = () => {
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
-            transition: { delay: 1.5, duration: 0.4, ease: "easeIn" },
+            transition: { delay: 2.0, duration: 0.4, ease: "easeIn" },
           }}
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] absolute"
         >
