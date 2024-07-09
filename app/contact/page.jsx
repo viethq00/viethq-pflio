a469e519c345b5443c9325c1c90443fd3321421c
viethq00 <73158024+viethq00@users.noreply.github.com>
2024-07-09 08:48:15 +0700

    update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -48,8 +48,8 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[38px]">
           <div className="xl:h-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
-              <p className="text-white/60">Let's work together...</p>
+              <h3 className="text-4xl text-accent">Work together</h3>
+              <p className="text-white/60">Work together...</p>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <Input type="firstname" placeholder="Firstname" />
                 <Input type="lastname" placeholder="Lastname" />
--- app/page.jsx
+++ app/page.jsx
@@ -12,10 +12,11 @@ export default function Home() {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br /> <span className="text-accent">Ha Quoc Viet</span>
+              Hello I am <br />{" "}
+              <span className="text-accent">Ha Quoc Viet</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80 ">
-              I'm a software developer based in Vietnam. I specialize in
+              I am a software developer based in Vietnam. I specialize in
               building exceptional digital experiences.
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
