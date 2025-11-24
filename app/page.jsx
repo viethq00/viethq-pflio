d39175b3ddf03cd1cdec2468a8da6cc1ba66243e
vincentFpt <73158024+viethq00@users.noreply.github.com>
2025-11-25 00:09:12 +0700

    ok

--- app/page.jsx
+++ app/page.jsx
@@ -6,10 +6,6 @@ import Stats from "@/components/Stats";
 import { FiDownload } from "react-icons/fi";
 
 export default function Home() {
-  const downloadCV = () => {
-    const fileUrl = "/assets/CV_HaQuocViet_SoftwareEngineer.pdf";
-    window.open(fileUrl, "_blank");
-  };
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
@@ -33,15 +29,22 @@ export default function Home() {
             </p>
 
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
-                className="uppercase flex items-center gap-2"
-                onClick={downloadCV}
+              <a
+                href="/assets/CV_HaQuocViet_SeniorSE.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                download="CV_HaQuocViet_SeniorSE.pdf"
+                className="inline-block"
               >
-                <span>Download CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2"
+                >
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
                 <Socials
                   containerStyles="flex gap-6"
