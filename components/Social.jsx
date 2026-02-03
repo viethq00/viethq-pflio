f09a1c348321b89f342f37a2e49da69daad45108
viethq <73158024+viethq00@users.noreply.github.com>
2026-02-04 00:03:33 +0700

    asdasd

--- components/Social.jsx
+++ components/Social.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const socials = [
   {
     icon: <FaLinkedinIn />,
-    path: "https://www.linkedin.com/in/qu%E1%BB%91c-vi%E1%BB%87t-h%C3%A0-6bbb24244/",
+    path: "https://www.linkedin.com/in/viethq00/",
   },
   { icon: <FaGithub />, path: "https://github.com/viethq00" },
 ];
